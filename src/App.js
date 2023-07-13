e692ec87fc61a0be45e6fa2d04fe21f010c62504
Eugene <eugenevasile@gmail.com>
2023-07-13 16:58:36 +0200

    Small update
    Small update

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
 
 	return (
-		<div className="h-screen grid grid-cols-1 place-content-center">
-			<h1 className='text-center'>Weclome to Eugene's corner.</h1>
+		<div className="h-screen bg-zinc-50 dark:bg-black grid grid-cols-1 place-content-center">
+			<h1 className="text-center text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem]">Welcome to Eugene's Corner.</h1>
+			<h2 className="py-6 text-center text-2xl font-semibold leading-6 text-slate-900">A place for software developers and machine learning enthusiasts.</h2>
   		</div>
   	);
 }
