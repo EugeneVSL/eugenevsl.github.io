484e21cbab82488ae6a9aac8ea128262f0bf9263
Eugene <eugenevasile@gmail.com>
2023-07-14 21:10:37 +0200

    Updated the favicon.ico
    Updated the favicon.ico

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
             <Header />
             <main className="inner cover">
                 <h2 className="">Welcome to Eugene's Corner.</h2>
- 				<h3 className="">A place for software developers infected with machine learning.</h3>
+ 				<h3 className="">A place for software developers infected with the AI bug.</h3>
             </main>
             <Footer />
         </>
