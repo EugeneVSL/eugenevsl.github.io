42a6efb0203d066f2fb1ec742e0408b5edc38aa0
Eugene <eugenevasile@gmail.com>
2023-07-14 09:36:12 +0200

    Showed some love to the landing page
    Showed some love to the landing page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
 
 	return (
-		<div className="h-screen bg-zinc-50 dark:bg-black grid grid-cols-1 place-content-center">
-			<h1 className="text-center text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem]">Welcome to Eugene's Corner.</h1>
-			<h2 className="py-6 text-center text-2xl font-semibold leading-6 text-slate-900">A place for software developers and machine learning enthusiasts.</h2>
-  		</div>
+		<>
+			<header className="masthead mb-auto"></header>
+			<main className="inner cover">
+				<h1 className="">Welcome to Eugene's Corner.</h1>
+				<h2 className="">A place for software developers and machine learning enthusiasts.</h2>
+			</main>
+			<footer className="mastfoot mt-auto"></footer>
+		</>
   	);
 }
 
