081be30275dce51f5df8ece8000f5adcc602ad8a
Eugene <eugenevasile@gmail.com>
2023-07-23 13:02:28 +0200

    Added the navigation in the header
    Added the navigation in the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,17 +6,56 @@ import React from "react";
 
 export const Header = () => {
 
-    return (
-        <header className="masthead mb-auto">
-            <a href="/" className="d-flex align-items-center text-decoration-none">
-                <img src={logo} className="me-3 rounded" alt="logo" width='70px' />
-                <h1 className="float-md-start mb-0">The Development Task</h1>
-            </a>
-            {/* <nav className="nav nav-masthead justify-content-center float-md-end">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Contact</a>
-            </nav>   */}
-        </header>
-    )
+  return (
+    <header className="masthead">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex align-items-center text-decoration-none" href="/">
+            <img src={logo} className="rounded" alt="logo" width='70px' />
+            <h1 className="float-md-start mb-0">The Development Task</h1>
+          </a>
+          
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {/* <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li> */}
+              {/* <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li> */}
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
+        {/* <a href="/" className="d-flex align-items-center text-decoration-none">
+            <img src={logo} className="me-3 rounded" alt="logo" width='70px' />
+            <h1 className="float-md-start mb-0">The Development Task</h1>
+        </a> */}
+        {/* <nav className="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+        </nav>   */}
+    </header>
+  )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,10 +9,12 @@ export const Home = () => {
     return (
         <>
             <Header />
-            <main className="inner cover">
+
+            <main className="py-5 text-center container">
                 <h2 className="">Welcome to Eugene's Corner.</h2>
  				<h3 className="">A place for software developers infected with the AI bug.</h3>
             </main>
+            
             <Footer />
         </>
     )
